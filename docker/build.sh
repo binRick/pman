d404521685c7a407ca2f893dc655cafcebc4cf4f
rick <rick@ricks-MacBook-Air.local>
2022-05-25 00:20:13 -0400

    m

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,8 @@
-docker build . -t pman
+#!/usr/bin/env bash
+set -eou pipefail
+cd "$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+
+rsync -ar ../submodules .
+rsync -ar ../deps .
+
+docker build . -t pman --target pman
--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,7 @@
-./build.sh
+#!/usr/bin/env bash
+set -eou pipefail
+cd "$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+#./build.sh
 docker run \
     -v $(eval echo ~/.ssh/id_rsa:/root/.ssh/id_rsa):ro \
     -w /root/repos/pman \
